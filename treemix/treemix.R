01e412e71d6f871c7818ce9246a0f10380d9f5a2
Ryan Lynch <rlynch@colorado.edu>
2023-11-14 16:28:25 -0700

    Update treemix.R

--- treemix/treemix.R
+++ treemix/treemix.R
@@ -1,3 +1,6 @@
+#not sure where this saves the image file
+#some of the tree tip lables and migration color scale bar probably need to be hand edited for final draft 
+
 source("plottingfuncs.R")
 
 plot_tree("out_stem")
