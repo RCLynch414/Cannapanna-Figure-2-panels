f7a836e83d88aed0f0b17c73482439c4af89674f
Ryan Lynch <rlynch@colorado.edu>
2024-01-08 15:48:49 -0700

    Update fst_manhatten.R

--- Fst_manhatten/fst_manhatten.R
+++ Fst_manhatten/fst_manhatten.R
@@ -2,4 +2,4 @@
 hemp_mj_fsts <- read.csv("MJ_v_hemp.out.weir.fst.CMPlot_10000bp.txt", header = TRUE)
 library("CMplot")
 
-CMplot(hemp_mj_fsts,type="p",plot.type="m",col=c("grey30","grey60"),LOG10=FALSE,bin.size=1000000,chr.labels=paste("Chr",c(1:9,"X"),sep=""),r=0.4,cir.axis=TRUE,threshold=c(0.9), threshold.lty=c(1,2), threshold.lwd=c(2), threshold.col=c("black"),signal.col=c("green"), signal.cex=c(2),signal.pch=c(19),ylim=(c(0.2,1)), ylab=("Fst"), file="jpg",file.name="",dpi=300,file.output=TRUE,verbose=TRUE,width=10,height=10)
+CMplot(hemp_mj_fsts,type="p",plot.type="m",col=c("grey30","grey60"),LOG10=FALSE,bin.size=1000000,chr.labels=paste(c(1:9,"X"),sep=""),r=0.4,cir.axis=TRUE,threshold=c(0.9), threshold.lty=c(1,2), threshold.lwd=c(2), threshold.col=c("black"),signal.col=c("green"), cex=c(0.65), amplify=TRUE, signal.cex=c(0.75),signal.pch=c(19),ylim=(c(0.4,1)), ylab=("Fst"), axis.cex=1, file="jpg",file.name="",dpi=300,file.output=TRUE,verbose=TRUE,width=6,height=6)
